6085700b72504b4c6e1db0b73bf45e15f2fb1566
Prasad Reddy <prasad.kalyanreddy@gmail.com>
2024-07-12 18:32:46 +0530

    scripts

---
+++ deploy.ts
@@ -0,0 +1,18 @@
+import { ethers } from "hardhat";
+
+async function main() {
+  const ContractFactory = await ethers.getContractFactory("Doge");
+
+  // TODO: Set addresses for the contract arguments below
+  const instance = await ContractFactory.deploy(initialOwner);
+  await instance.waitForDeployment();
+
+  console.log(`Contract deployed to ${await instance.getAddress()}`);
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
